033fe5eea93ab0aaf0bb143702a92b7936b94477
topu <ardamax646@gmail.com>
2023-06-07 00:09:15 +0600

    added login and register page

--- app/src/main/java/com/example/cardiac_recorder_app/Splash_Activity.java
+++ app/src/main/java/com/example/cardiac_recorder_app/Splash_Activity.java
@@ -2,7 +2,9 @@ package com.example.cardiac_recorder_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_Activity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Splash_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splash_Activity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },1500);
+
     }
 }
