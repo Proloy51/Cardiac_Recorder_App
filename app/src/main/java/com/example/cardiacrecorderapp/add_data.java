1290f59d4153b4939e3402cb0582e1620d259b28
Proloy Karmakar <proloy@gmail.com>
2023-06-06 23:53:16 +0600

    Doing java code of Add record activity

--- app/src/main/java/com/example/cardiacrecorderapp/add_data.java
+++ app/src/main/java/com/example/cardiacrecorderapp/add_data.java
@@ -3,9 +3,12 @@ package com.example.cardiacrecorderapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class add_data extends AppCompatActivity {
 
+    private Button uploadbutton;
+    private 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
