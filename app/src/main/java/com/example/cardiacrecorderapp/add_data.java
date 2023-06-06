42d6853f2599bb1ec4925aa3edcb7b978412f40e
Proloy Karmakar <proloy@gmail.com>
2023-06-06 22:34:24 +0600

    Home page alomost done

---
+++ app/src/main/java/com/example/cardiacrecorderapp/add_data.java
@@ -0,0 +1,14 @@
+package com.example.cardiacrecorderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_data extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_data);
+    }
+}
